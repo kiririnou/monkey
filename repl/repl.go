9da13520c62b168fcf607a5a61944aad4a519da4
kiririnou <kou.pass94@gmail.com>
2024-03-07 00:44:37 +0200

    feat: added evaluator to repl

--- repl/repl.go
+++ repl/repl.go
@@ -1,11 +1,12 @@
 package repl
 
 import (
-    "bufio"
-    "fmt"
-    "io"
-    "monkey/lexer"
-    "monkey/parser"
+	"bufio"
+	"fmt"
+	"io"
+	"monkey/evaluator"
+	"monkey/lexer"
+	"monkey/parser"
 )
 
 const PROMPT = ">> "
@@ -30,8 +31,11 @@ func Start(in io.Reader, out io.Writer) {
             continue
         }
 
-        io.WriteString(out, program.String())
-        io.WriteString(out, "\n")
+        evaluated := evaluator.Eval(program)
+        if evaluated != nil {
+            io.WriteString(out, evaluated.Inspect())
+            io.WriteString(out, "\n")
+        }
     }
 }
 
