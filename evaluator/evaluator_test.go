2989102603699aac7336a1c8061514d89654d7f9
kiririnou <kou.pass94@gmail.com>
2024-03-07 19:15:47 +0200

    feat: added evaluation of if else expression

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -81,24 +81,24 @@ func TestEvalBooleanExpression(t *testing.T) {
         { "(1 < 2) == true", true },
         { "(1 < 2) == false", false },
         { "(1 > 2) == true", false },
-        { "(1 < 2) == false", true },
+        { "(1 > 2) == false", true },
     }
 
     for _, tt := range tests {
         evaluated := testEval(tt.input)
-        testBooleanObject(t, evaluated, tt.expected)
+        testBooleanObject(t, evaluated, tt.expected, tt.input)
     }
 }
 
-func testBooleanObject(t *testing.T, obj object.Object, expected bool) bool {
+func testBooleanObject(t *testing.T, obj object.Object, expected bool, input string) bool {
     result, ok := obj.(*object.Boolean)
     if !ok {
         t.Errorf("object is not Boolean. got=%T (%+v)", obj, obj)
         return false
     }
     if result.Value != expected {
-        t.Errorf("object has wrong value. got=%t, want=%t",
-            result.Value, expected)
+        t.Errorf("object has wrong value. got=%t, want=%t, input=%s",
+            result.Value, expected, input)
         return false
     }
     return true
@@ -119,6 +119,39 @@ func TestBangOperator(t *testing.T) {
 
     for _, tt := range tests {
         evaluated := testEval(tt.input)
-        testBooleanObject(t, evaluated, tt.expected)
+        testBooleanObject(t, evaluated, tt.expected, tt.input)
     }
+}
+
+func TestIfElseExpression(t *testing.T) {
+    tests := []struct {
+        input       string
+        expected    interface{}
+    }{
+        { "if (true) { 10 }", 10 },
+        { "if (false) { 10 }", nil },
+        { "if (1) { 10 }", 10 },
+        { "if (1 < 2) { 10 }", 10 },
+        { "if (1 > 2) { 10 }", nil },
+        { "if (1 > 2) { 10 } else { 20 }", 20 },
+        { "if (1 < 2) { 10 } else { 20 }", 10 },
+    }
+
+    for _, tt := range tests {
+        evaluated := testEval(tt.input)
+        integer, ok := tt.expected.(int)
+        if ok {
+            testIntegerObject(t, evaluated, int64(integer))
+        } else {
+            testNullObject(t, evaluated)
+        }
+    }
+}
+
+func testNullObject(t *testing.T, obj object.Object) bool {
+    if obj != NULL {
+        t.Errorf("object is not NULL. got=%T (%+v)", obj, obj)
+        return false
+    }
+    return true
 }
